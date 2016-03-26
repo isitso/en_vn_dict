c3378ab0d44783b589c5b95c86831e4e95c2465c
isitso <ryan033111@gmail.com>
2016-03-26 00:43:37 -0700

    add filter by character

--- en/urls.py
+++ en/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.WordView.as_view(), name='index'),
+    url(r'^([\w-]+)/$', views.WordView.as_view(), name='index-char'),
 ]
--- en/views.py
+++ en/views.py
@@ -9,3 +9,9 @@ class WordView(generic.ListView):
     """
     model = models.Word
     template_name = 'en/index.html'
+
+    def get_queryset(self):
+        char = self.args[0]
+        if char != None:
+            return models.Word.objects.filter(word__startswith=char)
+        return super().get_queryset()
